d83233d7ca16cf839a2421eb0c3ed0825f710f74
desfate <yujieworking@gmail.com>
2019-12-02 10:33:11 +0800

    java8 stream support

--- app/src/main/java/com/defate/java8support/MainActivity.java
+++ app/src/main/java/com/defate/java8support/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 
 import com.defate.j8s.J8s;
 import com.defate.j8s.interfaces.BiConsumer;
-import com.defate.j8s.interfaces.Consumer;
 import com.defate.j8s.interfaces.Predicate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
-import java.util.stream.Stream;
+
+import java9.util.function.Consumer;
+import java9.util.function.Function;
+import java9.util.stream.Stream;
+import java9.util.stream.StreamSupport;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<String> list = new ArrayList<>();
+        list.add("10");
+        list.add("5");
+        list.add("9");
         J8s.filter(list, new Predicate<String>() {
             @Override
             public boolean test(String s) {
@@ -31,5 +37,8 @@ public class MainActivity extends AppCompatActivity {
         HashMap<String, String> map = new HashMap<>();
         map.put("aaa", "bbb");
         map.put("ccc", "ddd");
+
+        //这个库可以向下兼容Stream库
+        StreamSupport.stream(list).map(Integer::valueOf).forEach(System.out::println);
     }
 }
